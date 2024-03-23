511a9bcb1d1401e733b817beef700c05a58c2cd5
papunidze <papunidze07@gmail.com>
2024-03-23 03:24:36 -0700

    feat:auth

---
+++ frontend/src/pages/auth/index.tsx
@@ -0,0 +1,19 @@
+const Auth = () => {
+  return (
+    <section className="h-full w-full flex items-center justify-center">
+      <div className="bg-primary-light flex w-full min-h-96 container px-6 py-14 rounded-lg shadow-sm gap-4">
+        <div className="flex-1 md:block hidden ">
+          {/* <img
+            src="https://firebasestorage.googleapis.com/v0/b/pchat-2442c.appspot.com/o/1710669141950.webp?alt=media"
+            alt="Company Logo"
+            className="object-cover rounded-lg shadow-sm"
+          /> */}
+          Image
+        </div>
+        <div className="flex-1">Form</div>
+      </div>
+    </section>
+  );
+};
+
+export default Auth;
